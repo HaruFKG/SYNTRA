6de0a2b7d857e013b28c9aadb1667ac3cf42a491
HaruFK <gabrielalvesgirardi@gmail.com>
2025-11-19 20:54:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function CriarForm(){
-    document.getElementById("Form").innerHTML = "<form name='Personagem'><input type='text' name='Nick' id='Nome' placeholder='NickName'><button onclick='MostrarDados(event)'>Enviar</button></form>";
-}
+$("#Iniciar").click(function() {
+    $("#Form").show();
+    $("#MenuIniciar").hide();
+    document.getElementById("Form").innerHTML = "<form name='Personagem'>"+
+    "<input type='text' name='Nick' id='Nome' placeholder='NickName'>"+
+    "<button onclick='MostrarDados(event)'>Enviar</button>"+
+    "</form> <button id='Voltar'>Voltar</button>";
+});
+$(document).on("click", "#Voltar", function() {
+    $("#Form").hide();
+    $("#MenuIniciar").show();
+});
 
 function MostrarDados(event){
     event.preventDefault();
     let nome = document.getElementById("Nome").value;
     document.getElementById("Form").innerHTML = `<h1>${nome}</h1>`
 }
+
